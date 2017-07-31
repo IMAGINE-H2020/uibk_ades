9c6898e1b9065f7abdd15afcd8e7c8832e74dcb2
Erwan <r1d1@github.com>
2017-07-31 15:16:24 +0200

    Adding generic header to ease use in external software

---
+++ include/libades.h
@@ -0,0 +1,10 @@
+// main header for external inclusion
+
+#include "storage/adesdb.h"
+
+#include "types/ades.h"
+#include "types/motion.h"
+#include "types/motion_sequence.h"
+#include "types/motion_type.h"
+
+#include "utils/serializable.h"
