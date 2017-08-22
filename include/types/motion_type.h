390cccebc05e41c862753ec96b06f031581e33a8
Erwan <r1d1@github.com>
2017-08-22 16:27:53 +0200

    Prepare potential implementation of conjugate of typeToString

--- include/types/motion_type.h
+++ include/types/motion_type.h
@@ -11,5 +11,6 @@ namespace ades {
 
     //std::string typeToString(int mt);
     std::string typeToString(enum MotionType mt);
+    // Future implementation enum typeToString(enum MotionType mt);
 
 }
