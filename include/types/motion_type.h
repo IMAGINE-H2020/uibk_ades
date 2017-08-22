4d25783e000391cb6671502d49026af52328f735
Erwan <r1d1@github.com>
2017-08-22 11:51:06 +0200

    Adds a way to get human-readable MotionType info

--- include/types/motion_type.h
+++ include/types/motion_type.h
@@ -1,10 +1,15 @@
 /*! \file motion_type.h */
 
 #pragma once
+#include <string>
 
 namespace ades {
 
+    const int motionTypeNb = 5;
      /*! Defining the types of movement representations currently support by libades. */
     enum class MotionType { DMP, Trajectory, Cyclic, VisualServoing, PID };
+
+    //std::string typeToString(int mt);
+    std::string typeToString(enum MotionType mt);
 
 }
---
+++ src/types/motion_type.cpp
@@ -0,0 +1,18 @@
+#include "../../include/types/motion_type.h"
+
+namespace ades {
+
+    std::string typeToString(enum MotionType mt)
+    {
+        int mt_ = static_cast<int>(mt);
+        const std::string types[motionTypeNb] ={"DMP", "Trajectory", "Cyclic", "VisualServoing", "PID"};
+        if((mt_ < 0) || (mt_ > motionTypeNb))
+        {
+            return "Unknown";
+        }
+        else
+        {
+            return types[mt_];
+        }
+    }
+}
